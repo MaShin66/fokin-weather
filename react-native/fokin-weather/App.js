37cd6145a949ce71ab1adf4ab17ab04461992a80
신호태 <ubiquitous6666@gmail.com>
2019-09-17 12:32:42 +0900

    3days

--- react-native/fokin-weather/App.js
+++ react-native/fokin-weather/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = "c8458517524180e85ee3cd47d5aa6022";
 
 export default class extends React.Component {
   
   state = {
     isLoading: true
+  }
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
   }
 
   getLocation = async () => {
@@ -16,9 +26,8 @@ export default class extends React.Component {
       await Location.requestPermissionsAsync();
       const { 
         coords: latitude, longitude } = await Location.getCurrentPositionAsync();
-        this.setState({
-          isLoading: false
-        });
+        this.getWeather(latitude, longitude);
+        this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("can't find you.", "So sad");
     }
